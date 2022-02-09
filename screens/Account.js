4d1f0b01b4a36af0c1900a2b5d39adff40cb4114
flawlesscode254 <denzme414@gmail.com>
2022-02-09 21:35:17 +0300

    account info added

--- screens/Account.js
+++ screens/Account.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -18,9 +18,32 @@ const Account = () => {
         }}
       >
         <Ionicons color={"#04e064"} name="person" size={100} />
-        <Text style={{
-            color: "#FFFFFF"
-        }}>Account</Text>
+        <Text
+          style={{
+            color: "#FFFFFF",
+          }}
+        >
+          Username
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#e00438",
+            paddingVertical: 5,
+            paddingHorizontal: 40,
+            borderRadius: 6,
+            alignItems: "center",
+            marginTop: 150,
+          }}
+        >
+          <Text
+            style={{
+              color: "#FFFFFF",
+              letterSpacing: 1,
+            }}
+          >
+            Log out
+          </Text>
+        </TouchableOpacity>
       </LinearGradient>
     </View>
   );
